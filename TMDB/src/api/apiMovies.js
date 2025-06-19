cbfb032f002c68f26fd9fa0bacadcc2f4c3a6825
RoarCriss <cristina94m@gmail.com>
2025-06-19 11:24:04 +0200

    group practice2

--- TMDB/src/api/apiMovies.js
+++ TMDB/src/api/apiMovies.js
@@ -1,4 +1,8 @@
 const baseMoviesUrl = "https://api.themoviedb.org/3/movie";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-//
+// req movies
+
+async function getAllMovies() {
+  const url = `${baseMoviesUrl}`;
+}
